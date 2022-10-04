a82096b884ba553294dc965b857b8cca28787753
beinsaduno <krastevtodordinev@yahoo.com>
2022-10-04 14:14:41 +0200

    Add P06_Functions

---
+++ cpp/S01_Introduction/P06_Functions/P06_Functions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#define SIZE 4
+
+int maxNum(int[]);
+
+int main() {
+
+	int num1, num2, num3, num4;
+	int array[SIZE];
+
+	for (size_t i = 0; i < SIZE; i++) {
+		std::cin >> array[i];
+	}
+
+	int result = maxNum(array);
+
+	std::cout << result << std::endl;
+
+	return 0;
+}
+
+int maxNum(int arr[]) {
+	int max = INT32_MIN;
+
+	for (size_t i = 0; i < SIZE; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+	}
+
+	return max;
+}
